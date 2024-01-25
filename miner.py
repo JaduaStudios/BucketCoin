09e15fff43f1da9892820496ceac7af22ed2f738
Jadua1010 <jmb-n@outlook.com>
2024-01-25 01:29:25 +0100

    Some updates

--- miner.py
+++ miner.py
@@ -2,29 +2,34 @@ import requests
 import hashlib
 import json
 import random
+import concurrent.futures
 
-def mine_crypto(balance):
-    # Simulate mining by generating a random hash
-    random_number = random.randint(0, 999999)
-    data_to_hash = str(random_number)
-    hash_result = hashlib.sha256(data_to_hash.encode()).hexdigest()
+def calculate_hash(data_to_hash):
+    return hashlib.sha256(data_to_hash.encode()).hexdigest()
 
-    # Check if the balance is 1, then add 1 to the balance and reset it to 0
-    if balance == 1:
-        balance = 0
-        print("Mining Successful! New hash:", hash_result)
-    else:
-        balance += 1
-        print("Mining Successful! New hash:", hash_result)
+def mine_crypto(username, password, balance, num_processes=61):
+    with concurrent.futures.ProcessPoolExecutor(max_workers=num_processes) as executor:
+        # Increase CPU usage by parallelizing hash calculations
+        random_numbers = [random.randint(0, 999999) for _ in range(100000)]
+        data_to_hashes = [str(random_number) for random_number in random_numbers]
 
-    return balance, hash_result
+        hash_results = list(executor.map(calculate_hash, data_to_hashes))
 
-def send_to_php_server(result):
+    # Use the last hash result as the final result
+    hash_result = hash_results[-1]
+
+    balance += 1
+    print("Mining Successful! New hash:", hash_result)
+
+    return balance, hash_result, username, password
+
+
+def send_to_php_server(result, username, password):
     # Define the PHP server URL
     php_server_url = 'https://drive.jaduastudios.com/temp/crypto/submit_balance.php'
 
-    # Create a dictionary with the result
-    payload = {'result': result}
+    # Create a dictionary with the result, username, and password
+    payload = {'result': result, 'username': username, 'password': password}
 
     # Send a POST request to the PHP server
     response = requests.post(php_server_url, data=payload)
@@ -36,8 +41,16 @@ if __name__ == "__main__":
     # Initial balance
     balance = 0
 
-    # 100% accurate mining
-    balance, mined_result = mine_crypto(balance)
+    # Replace with values from JSON
+    username = 'test'
+    password = 'test'
 
-    # Send the result to the PHP server
-    send_to_php_server(balance)
+    # Number of coins to mine (get from JSON)
+    num_iterations = 10
+
+    for _ in range(num_iterations):
+        # Simulate mining
+        balance, mined_result, username, password = mine_crypto(username, password, balance)
+        
+        # Send the hash to the PHP server
+        send_to_php_server(mined_result, username, password)
